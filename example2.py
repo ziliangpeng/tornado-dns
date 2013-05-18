57c2d73c5915d686e8c59b3043fe890339fe30f3
Victor <ziliangdotme@gmail.com>
2013-05-19 03:49:45 +0930

    updated example2.py - small tweak and tiny refactoring

--- example2.py
+++ example2.py
@@ -13,6 +13,7 @@ timeout_cnt = 0
 nonexist_cnt = 0
 crash_cnt = 0
 other_cnt = 0
+remain_item_cnt = 0
 
 l = threading.Lock()
 
@@ -25,42 +26,36 @@ def main():
             url = q.get(block=False)
             tornado_dns.lookup(url, success, errback, timeout=5000)
         except Queue.Empty:
-            io_loop.stop()
-            print 'success', success_cnt
-            print 'timeout', timeout_cnt
-            print 'nonexist', nonexist_cnt
-            print 'crash', crash_cnt
-            print 'other', other_cnt
-            end_time = datetime.datetime.now()
-            print 'time', end_time - start_time
-            pass
+            if not remain_item_cnt:
+                io_loop.stop()
 
     def success(addresses):
-        global success_cnt
+        global success_cnt, remain_item_cnt
         with l:
             print 'addresses: %s' % (addresses,)
-            end_time = datetime.datetime.now()
             success_cnt += 1
-            do_next()
             q.task_done()
+            remain_item_cnt -= 1
+            do_next()
 
     def errback(code):
         with l:
             if code == tornado_dns.errors.TIMEOUT:
                 global timeout_cnt
                 timeout_cnt += 1
-                print 'time out'
-            elif code == -1:
+            elif code == tornado_dns.errors.DOMAIN_NOT_EXIST:
                 global nonexist_cnt
                 nonexist_cnt += 1
-            elif code == -2:
+            elif code == tornado_dns.errors.DNS_SERVER_FAILED:
                 global crash_cnt
                 crash_cnt += 1
             else:
                 global other_cnt
                 other_cnt += 1
-            do_next()
             q.task_done()
+            global remain_item_cnt
+            remain_item_cnt -= 1
+            do_next()
 
     alpha = 'abcdefghijklmnopqrstuvwxyz'
     for a in alpha:
@@ -68,11 +63,21 @@ def main():
             for c in alpha:
                 for d in alpha:
                     q.put(a+b+c+d+'.com')
+                    global remain_item_cnt
+                    remain_item_cnt += 1
 
-    for i in range(100):
+    for i in range(1000):
         url = q.get()
         tornado_dns.lookup(url, success, errback, timeout=5000)
     io_loop.start()
+    print 'success', success_cnt
+    print 'timeout', timeout_cnt
+    print 'nonexist', nonexist_cnt
+    print 'crash', crash_cnt
+    print 'other', other_cnt
+    print 'all', success_cnt + timeout_cnt + nonexist_cnt + crash_cnt + other_cnt
+    end_time = datetime.datetime.now()
+    print 'time', end_time - start_time
 
 
 if __name__ == '__main__':
